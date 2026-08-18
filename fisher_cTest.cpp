998044fd7ce32f4054b7f2a86d746f4f1d06d6b6
Shaggy <47267279+shxggy3386@users.noreply.github.com>
2026-08-18 02:43:00 -0500

    Create fisher_cTest.cpp

---
+++ fisher_cTest.cpp
@@ -0,0 +1,51 @@
+// Tomorrowland
+{
+  // BE
+  {
+    // 2025
+    {
+      // 07-20_2138_FISHER.mp3
+
+      // 07-20_2138_FISHER
+      {
+        // 01-Chris Lorenzo - Appetite.mp3
+        // 02-FISHER & AR_CO - Ocean.mp3
+        // 03-Marlon Hoffstadt aka DJ Daddy Trance - It's That Time (FISHER Remix).mp3
+        // 04-Fred again.. & Skepta & PlaqueBoyMax - Victory Lap (HILLS Remix).mp3
+        // ...
+        // 27-Marlon Hoffstadt & Coach Harrison - Daddy's In The Club (FISHER Remix).mp3
+        // folder.jpg
+      }
+    }
+
+    // 2026
+    {
+      // tree.txt
+      // tree2.txt
+
+      // Video
+      {
+      }
+
+      // WE1
+      {
+        // tree_we1.txt
+        // WE1_tree.txt
+
+        // Atmosphere
+        {
+          // 07-17_1830_KUKO.mp3
+          // 07-17_2000_MANDY-b2b-Negativ.mp3
+          // 07-17_2300_Sara-Landry.mp3
+          // 07-18_2000_BIIA-b2b-Charlie-Sparks.mp3
+          // 07-18_2130_Indira-Paganotto.mp3
+          // 07-18_2330_Reinier-Zonneveld-(Live).mp3
+          // 07-19_1400_VE-RA.mp3
+          // 07-19_1730_SHDW-b2b-ÜBERKIKZ.mp3
+          // 07-19_1900_ØTTA.mp3
+          // 07-19_2030_Anetha.mp3
+        }
+      }
+    }
+  }
+}
